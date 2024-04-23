f52e7c81d326d5df65c574d085dee7881b96e329
Martyna Dudzińska <150166810+martynadudzinska@users.noreply.github.com>
2024-04-23 20:38:05 +0200

    change quizy

---
+++ src/pages/Quizy.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Quizy")
+st.subheader(" ")
+
+pages_names = ['Część 1', 'Część 2']
+page = st.radio('Wybierz zakres materiału', pages_names, index=None)
+
+if page == 'Część 1':
+    st.write("Pierwszaa")
+elif page == 'Część 2':
+    st.write("drugaaa")
