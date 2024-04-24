e28aa0415a6c7166ced0bb6bf25d221b916d0387
Martyna Dudzińska <150166810+martynadudzinska@users.noreply.github.com>
2024-04-24 17:47:05 +0200

    add reading answers

--- src/pages/Quizy.py
+++ src/pages/Quizy.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from src import db
 
 
 class Quizy:
@@ -11,55 +12,102 @@ class Quizy:
         st.subheader("Wybierz zakres materiału:")
 
         pages_names = ['Część 1', 'Część 2']
-        self.page = st.radio(label="Część 1. zawiera zakres materiału dr inż. Węsierskiej. Część 2. zawiera zakres dr inż. Polińskiego.", options=pages_names, index=0)
+        self.page = st.radio(
+            label="Część 1. zawiera zakres materiału dr inż. Węsierskiej. Część 2. zawiera zakres dr inż. Polińskiego.",
+            options=pages_names, index=0)
 
         if self.page == 'Część 1':
             self.get_questions(1)
         elif self.page == 'Część 2':
-            st.write("drugaaa")
+            self.get_questions(2)
 
     def get_questions(self, part):
         # funkcja łącząca z bazą w tym miejscu z argumentem = part
-        result = ((1, "radio", "Czy kotek ma cztery łapki?", 2, 1, "Prawda", "Fałsz"),
-                  (2, "select", "Które zwierzę jest ssakiem?", 4, 2, "Wróbelek", "Kotek", "Żabcia", "Ślimaczek"),
-                  (3, "check", "Które elementy posiada kotek?", 5, 124, "Uszka", "Pazurki", "Kopyta", "Ogonek",
-                   "Skrzydła"),
-                  (4, "radio", "Czy kotki lubią drapać?", 2, 1, "Bardzo", "Niezbyt"))
+        if part == 1:
+            result = ((1, "radio", "Czy kotek ma cztery łapki?", 2, 1, "Prawda", "Fałsz"),
+                      (2, "select", "Które zwierzę jest ssakiem?", 4, 2, "Wróbelek", "Kotek", "Żabcia", "Ślimaczek"),
+                      (3, "check", "Które elementy posiada kotek?", 5, 124, "Uszka", "Pazurki", "Kopyta", "Ogonek",
+                       "Skrzydła"),
+                      (4, "radio", "Czy kotki lubią drapać?", 2, 1, "Bardzo", "Niezbyt"))
+        elif part == 2:
+            result = ((1, "check", "Wybierz pory roku: ", 5, 1245, "Lato", "Wiosna", "Słońce", "Jesień", "Zima"),
+                      (2, "check", "Które z elementów to zwierzęta?", 4, 234, "Fitoplankton", "Meduza", "Zebra",
+                       "Koralowiec"),
+                      (3, "check", "Które grzyby są jadalne?", 4, 134, "Enoki", "Muchomor", "Pieczarka", "Boczniak"))
+        else:
+            result = None
 
+        length = len(result)
         self.answers = list(x for x in range(int(len(result))))
-        for r in result:
+
+        for index, r in enumerate(result):
             st.write(str(r[0]) + ". Pytanie:")
             cont = st.container(border=True)
             if r[1] == "radio":
-                self.radio_add(r, cont)
+                self.radio_add(r, cont, length)
             elif r[1] == "select":
-                self.select_add(r, cont)
+                self.select_add(r, cont, length)
             elif r[1] == "check":
-                self.check_add(r, cont)
+                self.check_add(r, cont, length)
 
-    def radio_add(self, q_tuple, c):
+    def radio_add(self, q_tuple, c, l):
         options = list(x for x in range(int(q_tuple[3])))
         for o in options:
             options[o] = str(q_tuple[5 + o])
         r_question = c.radio(
             str(q_tuple[2]),
-            options, index=None)
+            options, index=None
+        )
+        for i in range(int(q_tuple[3])):
+            if r_question == options[i]:
+                self.answers[int(q_tuple[0])-1] = 1+i
+            else:
+                self.answers[int(q_tuple[0]-1) - 1] = 0
 
-    def select_add(self, q_tuple, c):
+        st.write(self.answers)
+
+    def select_add(self, q_tuple, c, l):
         options = list(x for x in range(int(q_tuple[3])))
         for o in options:
             options[o] = str(q_tuple[5 + o])
         s_question = c.selectbox(
             str(q_tuple[2]),
-            options, index=None)
+            options, index=None, placeholder="Wybierz odpowiedź")
 
-    def check_add(self, q_tuple, c):
-        c.write(str(q_tuple[2]))
+        for i in range(int(q_tuple[3])):
+            if s_question == options[i]:
+                self.answers[int(q_tuple[0])-1] = 1+i
+            else:
+                self.answers[int(q_tuple[0]-1) - 1] = 0
+
+        st.write(self.answers)
+
+    def check_add(self, q_tuple, c, l):
+        c_answers = list(x for x in range(int(q_tuple[3])))
+
+        c.write(str(q_tuple[2])) #question
         options = list(x for x in range(int(q_tuple[3])))
         for o in options:
             options[o] = str(q_tuple[5 + o])
         for x in range(int(q_tuple[3])):
             check = c.checkbox(options[x])
+            if check:
+                c_answers[x] = "Checked"
+            else:
+                c_answers[x] = "N"
+
+        c.write(c_answers)
+        st.write(self.compress_check(c_answers))
+
+    def compress_check(self, ans):
+        number = 0
+        for index, a in enumerate(ans):
+            if a == "Checked":
+                number = number*10
+                number += index+1
+        return number
+
+
 
 
 quizy = Quizy()
